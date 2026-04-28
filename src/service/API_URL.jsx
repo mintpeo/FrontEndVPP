05687371d15c87978365935e8a5a03157e1f946c
mintpeo <115866214+mintpeo@users.noreply.github.com>
2026-04-28 10:39:25 +0700

    Update API_URL.jsx

--- src/service/API_URL.jsx
+++ src/service/API_URL.jsx
@@ -1,4 +1,4 @@
-export const API_URL = https://my-spring-boot-6j1w.onrender.com/;
+export const API_URL = "https://my-spring-boot-6j1w.onrender.com/";
 export const IS_LOGGED = "isLoggedIn";
 export const INFO_USER = "infoUser";
 export const QUANTITY_CART = "quantityCart";
